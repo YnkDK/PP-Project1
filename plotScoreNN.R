d321796e14cec00562dcf478160e950363b75a00
Martin Storgaard <20140000@post.au.dk>
2015-09-09 14:45:02 +0200

    Updated graph

--- plotScoreNN.R
+++ plotScoreNN.R
@@ -1,9 +1,16 @@
+library(lattice);
+
 args <- commandArgs(trailingOnly = TRUE)
 cumDist <- read.table(args[1])
 
 pdf(paste(args[1], ".pdf", sep = ""))
-plot(cumDist, main = "Cumulative Error Distribution",
+xyplot(V2 ~ V1, cumDist, main = "Cumulative Error Distribution",
     xlab = "Error in meters",
-    ylab = "Relative frequency"
+    ylab = "Relative frequency",
+    type="l",
+    grid = TRUE,
+    xlim = c(0, max(cumDist$V1)),
+    ylim = c(0, 1),
+    lwd = 4
 )
 dev.off()
